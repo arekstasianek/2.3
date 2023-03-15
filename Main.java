bcb47e3e2bd6ecf97579b9d7850b9546a80fc7b9
arekstasianek <126696173+arekstasianek@users.noreply.github.com>
2023-03-15 15:27:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+int sum = 0;
+        for (int i = 0; i <= 100; i += 2) {
+            sum += i;
+        }
+        System.out.println("Suma liczb parzystych od 0 do 100 wynosi: " + sum);
+    }
+}
